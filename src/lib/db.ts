17338021adbb011e3b5241dea7f7b90e2f99bff0
Joe Rall <joe@masterdevs.com>
2024-08-08 20:40:26 -0400

    Updated way to connect to planetscale

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,15 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
-import { connect } from "@planetscale/database";
+import { Client } from "@planetscale/database";
 import { env } from "~/env.mjs";
 import * as schema from "./schema";
 
 // create the connection
-const connection = connect({
+const client = new Client({
   host: env.MD_DATABASE_HOST,
   username: env.MD_DATABASE_USERNAME,
   password: env.MD_DATABASE_PASSWORD,
 });
 
-export const db = drizzle(connection, {
+export const db = drizzle(client, {
   schema,
 });
